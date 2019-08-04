113815bbe24737407754163a965930fad66e3902
Fran Zekan <zekan.fran369@gmail.com>
2019-08-04 18:25:55 +0200

    Init config

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <h1>Hello foobar</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
